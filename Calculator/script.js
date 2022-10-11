093966049c6031cbf849093b7406228ca6831373
Joseph Kim <kimg009263@gmail.com>
2022-10-11 23:56:43 +1300

    second commit

--- Calculator/script.js
+++ Calculator/script.js
@@ -15,11 +15,17 @@ class Calculator {
     }
 
     appendNumber(number) {
-        
+        if (number === '.' && this.currentOperand.includes('.')) return
+        debugger;
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+        debugger;
     }
 
     chooseOperation(operation) {
-        
+        if(this.currentOperand === '') return
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     compute() {
@@ -28,23 +34,32 @@ class Calculator {
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
+        
     }
 }
 
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
-const equalButtons = document.querySelectorAll('[data-equal]')
-const deleteButtons = document.querySelectorAll('[data-delete]')
-const allClearButtons = document.querySelectorAll('[data-all-clear]')
-const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
-const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
 })
